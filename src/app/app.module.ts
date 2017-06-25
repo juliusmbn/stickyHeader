fbc6ee28d39a0750dc81f3aa78580ff244257d57
juliusmbn <julius@mybizniche.com>
2017-06-25 08:55:30 +0800

    sticker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StickerComponent } from './sticker/sticker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StickerComponent
   ],
   imports: [
     BrowserModule
